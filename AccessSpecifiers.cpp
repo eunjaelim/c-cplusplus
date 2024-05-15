fe81e7b37730dd6fcdd1a6cec17314cebc8f5eb4
eunjaelim <imeunjae361@gmail.com>
2024-05-15 14:12:35 +0900

    2024-05-15

---
+++ AccessSpecifiers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+public myClass{
+
+public:
+    int x;
+private:
+    int y;
+
+}
+
+
+int main(){
+myClass myObj;
+myObj.x = 30;
+myObj.y =50;
+
+cout << myObj.x << endl;
+cout << myObj.y << endl;
+
+
+
+return 0;
+}
---
+++ Constructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass{
+public:
+    MyClass(){
+        cout << "hello World"<<endl;
+    }
+};
+
+int main(){
+MyClass myObj;
+
+
+
+
+
+return 0;
+}
